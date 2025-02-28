96fca4310f418fc5dafebd03f027cacc63330a90
Yurara <120286549+Yuraraexe@users.noreply.github.com>
2025-02-28 10:45:12 +0900

    Edit script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-const json = '{"name" = "Yurara", "age" = 15}';
-const obj = JSON.parse(json);
-print()
+const json = 
+{
+    "Yurara" : {
+        "name" : "Yurara",
+        "attack" : 10,
+        "defence": 5,
+        "hp" : 100
+    },
+    "Yuminty" : {
+        "name" : "Yuminty",
+        "attack" : 5,
+        "defence" : 10,
+        "hp" : 50
+    }
+}
